1a1b2ea471158542019da3bb658286c9d54afb9a
parzivail <colby.newman2000@gmail.com>
2017-03-06 10:57:03 -0500

    small animations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-	$("#search").autocomplete({
-		paramName: "",
+	var search = $("#search");
+
+	search.autocomplete({
 		serviceUrl: "http://keybind.parzivail.com/api/v1/key/program",
 		transformResult: function(response) {
 			return {
 				suggestions: JSON.parse(response)
 			};
+		}
+	});
+
+	search.keydown(function(e)
+	{
+		if (e.keyCode == 13)
+		{
+			$(".inputbox").css("top", "10%");
 		}
 	});
 });
